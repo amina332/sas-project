507f8f95492494702eb12fb5dfd827191b48d7e8
lahouel amina <aminalahouel9@gmail.com>
2026-09-17 12:02:01 +0200

    gfdrtg

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+function menu() {
+    let choix
+do {
+    console.log("===== SAS PROGRESS CONSOLE =====");
+    console.log("1. Afficher le tableau de bord");
+    console.log("2. Afficher la liste des apprenants");
+    console.log("3. Ajouter un apprenant");
+    console.log("4. Consulter un apprenant par identifiant");
+    console.log("5. Ajouter ou modifier le résultat d'une journée");
+    console.log("6. Rechercher un apprenant par nom");
+    console.log("7. Filtrer les apprenants par niveau");
+    console.log("8. Trier les apprenants par progression décroissante");
+    console.log("9. Trier les apprenants par ordre alphabétique");
+    console.log("0. Quitter");
+    choix = prompt("choisit une option:") 
+} while (choix !== "0");
+}
